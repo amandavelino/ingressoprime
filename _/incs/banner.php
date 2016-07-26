f05188139c9b1371471af13ce834abee57158ad6
amanda lopes <amanda lopes>
2016-07-25 22:39:06 -0300

    Alterações no site: incluindo imagens dos eventos.

--- _/incs/banner.php
+++ _/incs/banner.php
@@ -9,15 +9,15 @@
 				<article>
 					
 					<figure>
-						<img src="imgs/banner/banners-600x600-gwhite.jpg" border="0" alt="Garota White">
+						<img src="imgs/banner/garota-white-recife-vendas-ingresso-prime.jpg" border="0" alt="Garota White Recife">
 					</figure>
 
 					<div class="infoEvento all-transitions-out">
 						<header class="all-transitions-out">
-							<h3>Garota White</h3>
-							<h4>Recife - PE | Sex 15. Jul às 21:00</h4>						
+							<h3>Garota White Recife</h3>
+							<h4>Recife - PE | Sab 27. Ago às 22:00</h4>						
 						</header>
-						<p>A Festa conceito de Wesley Safadão, o GAROTA WHITE rodou o Brasil e chegará pela primeira vez em RECIFE! O evento será realizado dia 27 de Agosto na maior casa de shows da América Latina, CLASSIC HALL. O Anfitrião da festa WESLEY SAFADÃO é a principal atração e se destaca pelo palco 360º que garante visão privilegiada de qualquer ponto do local. Com todo público e o próprio Wesley vestidos de branco, a festa aposta na cor também na produção da sua cenografia, oferecendo um visual clean e diferenciado. Outras atrações irão completar o lineup e irão garantir energia de sobra, para não deixar ninguém parado.</p>
+						<p>A Festa conceito de Wesley Safadão, o GAROTA WHITE rodou o Brasil e chegará pela primeira vez em RECIFE! O evento será realizado dia 27 de Agosto na maior casa de shows da América Latina, CLASSIC HALL. O Anfitrião da festa WESLEY SAFADÃO é a principal atração e se destaca pelo palco 360º que garante visão privilegiada de qualquer ponto do local. Com todo público e o próprio Wesley vestidos de branco, a festa aposta na cor também na produção...</p>
 					</div><!-- /infoEvento -->
 
 				</article>
@@ -29,15 +29,15 @@
 				<article>
 					
 					<figure>
-						<img src="imgs/banner/banners-600x600-bmja.jpg" border="0" alt="Reveillon Ansioso Ecomariner">
+						<img src="imgs/banner/o-rappa-em-recife-vendas-ingresso-prime.jpg" border="0" alt="O Rappa em Recife">
 					</figure>
 
 					<div class="infoEvento all-transitions-out">
 						<header class="all-transitions-out">
-							<h3>Reveillon Ansioso Ecomariner</h3>
-							<h4>Recife - PE | Sex 01. Ago às 21:00</h4>						
+							<h3>O Rappa</h3>
+							<h4>Recife - PE | Sex 01. Out às 22:00</h4>						
 						</header>
-						<p>Pela Primeira vez em Caruaru a dupla HENRIQUE & JULIANO realizará um grandioso Show no Palladium. Levando multidões em seus shows, a expectativa é a melhor possível. O evento será realizado no dia 29 de julho a partir das 22h.</p>
+						<p>Dia 01 de Outubro a banda O RAPPA voltará a Recife fazendo o Show de Lançamento do seu DVD que foi gravado na Oficina Francisco Brennand. Será um Show Imperdível no Centro de Convenções de Pernambuco.</p>
 					</div><!-- /infoEvento -->
 
 				</article>
@@ -49,15 +49,15 @@
 				<article>
 					
 					<figure>
-						<img src="imgs/banner/banners-600x600-aero.jpg" border="0" alt="Garota VIP Sunset em João Pessoa">
+						<img src="imgs/banner/aerosmith-em-recife-vendas-ingresso-prime.jpg" border="0" alt="Aerosmith Style 2016">
 					</figure>
 
 					<div class="infoEvento all-transitions-out">
 						<header class="all-transitions-out">
-							<h3>Garota VIP Sunset em João Pessoa</h3>
-							<h4>João Pessoa - PB | Dom 28. Ago às 16:00</h4>						
+							<h3>Aerosmith Style 2016</h3>
+							<h4>Recife - PE | Sex 21. Out às 22:00</h4>						
 						</header>
-						<p>O Garota Vip Sunset é um dos eventos mais esperados do Ano. O evento será realizado em 28 de agosto, na Arena FestVerão com o anfitrião WESLEY SAFADÃO  e mais MATHEUS & KAUAN  e MÁRCIA FELLIPE. Um evento imperdível.</p>
+						<p>Com turnê marcada para rodar o Brasil em outubro deste ano, os fãs da banda Aerosmith já podem comprar seu ingresso. O grupo norte-americano desembarca no Recife no dia 21 de outubro, no Classic Hall, em Olinda. Os fãs irão relembrar grandes sucessos da carreira como Mama Kin, What It Takes, Last Child, Back in the Saddle, I Don’t Want to Miss a Thing, Janie’s Got a Gun’, Sweet Emotion, Walk This Way e Dream On. A banda está há 46 anos na estrada e desembarca no Recife pela sexta vez, prometendo agitar os admiradores.</p>
 					</div><!-- /infoEvento -->
 
 				</article>
--- _/incs/eventos.php
+++ _/incs/eventos.php
@@ -6,19 +6,17 @@
         <div class="a-group filter-button-group">
           <ul>
               <li><a data-filter="*" href="#">Todos os eventos</a></li>
-              <li><a data-filter=".destaques" href="#">destaques</a></li>
-              <li><a data-filter=".confirmados" href="#">Eventos confirmados</a></li>
-              <li><a data-filter=".reveilon" href="#">Reveilón</a></li>
-              <li><a data-filter=".verao" href="#">Verão</a></li>
+              <li><a data-filter=".em-breve" href="#">Em Breve</a></li>
+              <li><a data-filter=".Reveillon" href="#">Reveillon Confirmados</a></li>
+              <li><a data-filter=".verão" href="#">Verão</a></li>
               <li><a data-filter=".carnaval" href="#">Carnaval</a></li>
+              <li><a data-filter=".x-wesley-safadao" href="#">X Weskeys Safadão</a></li>
           </ul>
         </div><!-- /filter-button-group -->
 
         <div class="grid">
 
-            <div class="grid-sizer"></div>
-
-            <div class="grid-item destaques">
+            <div class="grid-item">
 
                 <div class="evento">
                 
@@ -26,13 +24,139 @@
                                     
                         <figure>
                             <div class="overlay">
-                                <p>Dia 08 de Julho a Arena Apipucos recebrá a festa Forró de Julho com as bandas Capim com Mel, Felipe & Gabriel, Manoel Netto, Tiago Giba, Felipe Barão, Trio Nós 4 e Trio Pé de Serra Intinerante.</p>
+                                <p>Pela Primeira vez em Caruaru a dupla HENRIQUE & JULIANO realizará um grandioso Show no Palladium. Levando multidões em seus shows, a expectativa é a melhor possível. O evento será realizado no dia 29 de julho a partir das 22h.</p>
                                 <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
                             </div><!-- /overlay --> 
-                            <img src="imgs/temp/eventos/arraiaseuantonio.jpg" border="0" alt="Arriá de Seu Antônio">
+                            <img src="imgs/banner/balada-sertaneja-em-caruaru-vendas-ingresso-prime.jpg" border="0" alt="Arriá de Seu Antônio">
                             <figcaption>
-                                    <h3>Arraiá de Seu Antônio</h3>
-                                    <h4>Sex 10. Jun e Sáb 11 Jun</h4>   
+                                    <h3>Balada Sertaneja</h3>
+                                    <h4>Sex 29 Jul 2016</h4>   
+                                    <h4>Caruaru - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item -->        
+
+            <div class="grid-item">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>O evento ocorrerá às 20:00 no Teatro Boa Vista.</p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/5--seco-em-recife-vendas-ingresso-prime.jpg" border="0" alt="5 A Seco">
+                            <figcaption>
+                                    <h3>5 A Seco</h3>
+                                    <h4>Sáb 30 Jul 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item --> 
+
+            <div class="grid-item">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>Dia 05 de Agosto o ARCÁDIA APIPUCOS receberá a super banda JOTA QUEST.  A festa será OpenBar e começará a partir das 23h. um Show Imperdível!
+                                <strong>* Prezados clientes, reforçamos que para este evento não ocorrerá troca na bilheteria do evento.</strong></p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/jota-quest-recife-vendas-ingresso-prime.jpg" border="0" alt="Jota Quest">
+                            <figcaption>
+                                    <h3>Jota Quest</h3>
+                                    <h4>Sex 05 Ago 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item -->   
+
+            <div class="grid-item">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>Caruaru receberá na Área Externa do Polo Comercial o show da super banda CAPITAL INICIAL no dia 19 de Agosto a partir das 21h. Um show imperdível.</p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/capital-inicial-em-caruaru-vendas-ingresso-prime.jpg" border="0" alt="Capital Inicial">
+                            <figcaption>
+                                    <h3>Capital Inicial</h3>
+                                    <h4>Sex 19 Ago 2016</h4>   
+                                    <h4>Caruaru - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item -->   
+
+            <div class="grid-item">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>Recife vai receber a TNT PARTY, uma festa incrível que será realizada no HELIPONTO. A festa começa as 16h com num local único com uma Vibe espetacular. Os Shows ficam por conta de SAULO FERNANDES, DORGIVAL DANTAS e AVINE VINNY.</p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/tnt-party-recife-vendas-ingresso-prime.jpg" border="0" alt="TNT Party Recife">
+                            <figcaption>
+                                    <h3>TNT Party</h3>
+                                    <h4>Sáb 20 Ago 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item --> 
+
+            <div class="grid-item x-wesley-safadao">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>A Festa conceito de Wesley Safadão, o GAROTA WHITE rodou o Brasil e chegará pela primeira vez em RECIFE! O evento será realizado dia 27 de Agosto na maior casa de shows da América Latina, CLASSIC HALL. O Anfitrião da festa WESLEY SAFADÃO é a principal atração e se destaca pelo palco 360º que garante visão privilegiada de qualquer ponto do local. Com todo público e o próprio Wesley vestidos de branco, a festa aposta na cor também na produção da sua cenografia, oferecendo um visual clean e diferenciado...</p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/garota-white-recife-vendas-ingresso-prime.jpg" border="0" alt="Garota White Recife">
+                            <figcaption>
+                                    <h3>Garota White Recife</h3>
+                                    <h4>Sáb 27 Ago 2016</h4>   
                                     <h4>Recife - PE</h4>                            
                                 </figcaption>
                         </figure>
@@ -43,105 +167,107 @@
                 
             </div><!--/grid-item -->
 
-            <div class="grid-item confirmados">
+            <div class="grid-item">
 
                 <div class="evento">
-                    
+                
                     <article>
                                     
                         <figure>
                             <div class="overlay">
-                                <p>Pela Primeira vez em Caruaru a dupla HENRIQUE & JULIANO realizará um grandioso Show no Palladium. Levando multidões em seus shows, a expectativa é a melhor possível. O evento será realizado no dia 29 de julho a partir das 22h.</p>
-                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
-                            </div><!-- /overlay -->                 
-                            <img src="imgs/temp/eventos/baladasertaneja.jpg" border="0" alt="Arriá de Seu Antônio">
-                            <figcaption>
-                                <h3>Balada Sertaneja - Henrique & Juliano</h3>
-                                <h4>Sex 29 Jul às 22h</h4>
-                                <h4>Caruaru - PE</h4>                               
-                            </figcaption>
-                        </figure>
-
-                    </article>
-
-                </div><!-- /evento -->                      
-                
-            </div><!--/grid-item -->                                
-
-            <div class="grid-item destaques confirmados">
-                
-                <div class="evento">
-                    
-                    <article>
-                                    
-                        <figure>
-                            <div class="overlay">
-                                <p>Pela Primeira vez em Caruaru a dupla HENRIQUE & JULIANO realizará um grandioso Show no Palladium. Levando multidões em seus shows, a expectativa é a melhor possível. O evento será realizado no dia 29 de julho a partir das 22h.</p>
+                                <p>Nos dias 16 e 17 de Setembro a VAQUEJADA DE SURUBIM receberá as maiores atrações do país. Já são 79 anos de história da Maior Vaquejada do Brasil. Uma festa imperdível que você já pode garantir seu ingresso. Confira a Grade de Atrações:<br>
+                                <strong>16/09 Sexta: WESLEY SAFADÃO, DORGIVAL DANTAS e MÁRCIA FELLIPE</strong><br>
+                                <strong>17/09 Sábado: JORGE & MATEUS, GABRIEL DINIZ e PEDRINHO PEGAÇÃO</strong></p>
                                 <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
                             </div><!-- /overlay --> 
-                            <img src="imgs/temp/eventos/saojoaocapita.jpg" border="0" alt="Arriá de Seu Antônio">
+                            <img src="imgs/banner/vaauejada-surubim-vendas-ingresso-prime.jpg" border="0" alt="Vaquejada de Surubim">
                             <figcaption>
-                                <h3>São João da Capitá</h3>
-                                <h4>Sex 24 e Sáb 25 Jun às 21h</h4>
-                                <h4>Recife - PE</h4>                                
-                            </figcaption>
+                                    <h3>Vaquejada de Surubim</h3>
+                                    <h4>Sex 16 Set | Sáb 17 Set 2016</h4>   
+                                    <h4>Surubim - PE</h4>                            
+                                </figcaption>
                         </figure>
 
                     </article>
 
                 </div><!-- /evento -->  
-
-            </div><!--/ grid-item -->
-
-            <div class="grid-item reveilon">
                 
+            </div><!--/grid-item -->  
+
+            <div class="grid-item">
+
                 <div class="evento">
-                    
+                
                     <article>
-                                
+                                    
                         <figure>
                             <div class="overlay">
-                                <p>Dia 08 de Julho a Arena Apipucos recebrá a festa Forró de Julho com as bandas Capim com Mel, Felipe & Gabriel, Manoel Netto, Tiago Giba, Felipe Barão, Trio Nós 4 e Trio Pé de Serra Intinerante.</p>
+                                <p>Dia 01 de Outubro a banda O RAPPA voltará a Recife fazendo o Show de Lançamento do seu DVD que foi gravado na Oficina Francisco Brennand. Será um Show Imperdível no Centro de Convenções de Pernambuco.</p>
                                 <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
-                            </div><!-- /overlay -->     
-                            <img src="imgs/temp/eventos/arraiaseuantonio.jpg" border="0" alt="Arriá de Seu Antônio">
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/o-rappa-em-recife-vendas-ingresso-prime.jpg" border="0" alt="O Rappa em Recife">
                             <figcaption>
-                                <h3>Arraiá de Seu Antônio</h3>
-                                <h4>Sex 10. Jun e Sáb 11 Jun</h4>   
-                                <h4>Recife - PE</h4>                            
-                            </figcaption>
+                                    <h3>O Rappa</h3>
+                                    <h4>Sáb 01 Out 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
                         </figure>
 
                     </article>
 
-                </div><!-- /evento -->                          
-
-            </div><!--/ grid-item -->
-
-            <div class="grid-item verao">
+                </div><!-- /evento -->  
                 
+            </div><!--/grid-item -->  
+
+            <div class="grid-item">
+
                 <div class="evento">
-                    
+                
                     <article>
-                                
+                                    
                         <figure>
                             <div class="overlay">
-                                <p>Dia 08 de Julho a Arena Apipucos recebrá a festa Forró de Julho com as bandas Capim com Mel, Felipe & Gabriel, Manoel Netto, Tiago Giba, Felipe Barão, Trio Nós 4 e Trio Pé de Serra Intinerante.</p>
+                                <p>Com turnê marcada para rodar o Brasil em outubro deste ano, os fãs da banda Aerosmith já podem comprar seu ingresso. O grupo norte-americano desembarca no Recife no dia 21 de outubro, no Classic Hall, em Olinda. Os fãs irão relembrar grandes sucessos da carreira como Mama Kin, What It Takes, Last Child, Back in the Saddle, I Don’t Want to Miss a Thing, Janie’s Got a Gun’, Sweet Emotion, Walk This Way e Dream On. A banda está há 46 anos na estrada e desembarca no Recife pela sexta vez, prometendo agitar os admiradores.</p>
                                 <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
-                            </div><!-- /overlay -->     
-                            <img src="imgs/temp/eventos/arraiaseuantonio.jpg" border="0" alt="Arriá de Seu Antônio">
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/aerosmith-em-recife-vendas-ingresso-prime.jpg" border="0" alt="Aerosmith Style 2016">
                             <figcaption>
-                                <h3>Arraiá de Seu Antônio</h3>
-                                <h4>Sex 10. Jun e Sáb 11 Jun</h4>   
-                                <h4>Recife - PE</h4>                            
-                            </figcaption>
+                                    <h3>Aerosmith Style 2016</h3>
+                                    <h4>Sex 21 Out 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
                         </figure>
 
                     </article>
 
-                </div><!-- /evento -->                          
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item -->   
 
-            </div><!--/ grid-item -->            
+            <div class="grid-item">
+
+                <div class="evento">
+                
+                    <article>
+                                    
+                        <figure>
+                            <div class="overlay">
+                                <p>Pela primeira vez na capital pernambucana, está CONFIRMADO a realização de um dos maiores festivais de Música Sertaneja do Brasil: o FESTEJA RECIFE. A data será em 22 de Outubro. Mais informações serão divulgadas em breve.</p>
+                                <a href="#" class="btn btn-buy" title="Comprar Ingresso">Comprar Ingresso</a>
+                            </div><!-- /overlay --> 
+                            <img src="imgs/banner/festeja-recife-vendas-ingresso-prime.jpg" border="0" alt="Festeja Recife">
+                            <figcaption>
+                                    <h3>Festeja Recife</h3>
+                                    <h4>Sáb 22 Out 2016</h4>   
+                                    <h4>Recife - PE</h4>                            
+                                </figcaption>
+                        </figure>
+
+                    </article>
+
+                </div><!-- /evento -->  
+                
+            </div><!--/grid-item -->                                                                                                                
 
         </div><!--/grid -->
 
